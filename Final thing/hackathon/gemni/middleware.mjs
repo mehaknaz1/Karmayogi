a5dbe3f6a07d866bbd63f6654dd48b4cd951eac5
Stella Machado <machadovinish@gmail.com>
2025-09-20 16:06:22 +0530

    new file

--- Final thing/hackathon/gemni/middleware.mjs
+++
@@ -1,9 +0,0 @@
-import { aiBot } from "./gemni.mjs";
-
-export const middleware = async (req, res, next) => {
-  const { message } = req.body;
-  const result = await aiBot(message);
-  res.status(200).json({
-    message: result,
-  });
-};
